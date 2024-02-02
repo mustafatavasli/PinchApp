5a9fe544ba3e409baca3c5e3271cf3ec7582879a
mustafatavasli <musstafatavasli@gmail.com>
2024-02-02 11:10:01 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Mustafa TAVASLI on 2.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
