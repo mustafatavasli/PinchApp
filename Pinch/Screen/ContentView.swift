cfead6c92d6390908488f8cdab7251e1f03fdc26
mustafatavasli <musstafatavasli@gmail.com>
2024-02-02 11:31:36 +0300

    Project Setup Completed, Assets Added

---
+++ Pinch/Screen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Mustafa TAVASLI on 2.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
