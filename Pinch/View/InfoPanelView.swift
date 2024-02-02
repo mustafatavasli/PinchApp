385b6207c06b44da7de56ff0bdf12d68662dc909
mustafatavasli <musstafatavasli@gmail.com>
2024-02-02 23:41:48 +0300

    InfoPanelView Created, Long Press Gesture Added

---
+++ Pinch/View/InfoPanelView.swift
@@ -0,0 +1,68 @@
+//
+//  InfoPanelView.swift
+//  Pinch
+//
+//  Created by Mustafa TAVASLI on 2.02.2024.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    
+    var scale : CGFloat
+    var offset : CGSize
+    
+    @State private var isInfoPanelVisible : Bool = false
+    
+    var body: some View {
+        HStack {
+            // MARK: HOTSPOT
+            Image(systemName: "circle.circle")
+                .symbolRenderingMode(.hierarchical)
+                .resizable()
+                .frame(width: 30, height: 30)
+            
+                // Long Press Gesture - 1 sn basili tutunca
+                .onLongPressGesture(minimumDuration: 1) {
+                    withAnimation(.easeOut) {
+                        // Paneli gorunur hale getirme
+                        isInfoPanelVisible.toggle()
+                    }
+                }
+            Spacer()
+            
+            // MARK: INFO PANEL
+            HStack(spacing: 2) {
+                Image(systemName: "arrow.up.left.and.arrow.down.right")
+                Text("\(scale)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.left.and.right")
+                Text("\(offset.width)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.up.and.down")
+                Text("\(offset.height)")
+                
+                Spacer()
+            }
+            .font(.footnote)
+            .padding(8)
+            .background(.ultraThinMaterial)
+            .cornerRadius(8)
+            .frame(maxWidth: 420)
+            .opacity(isInfoPanelVisible ? 1 : 0)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    InfoPanelView(scale: 1, offset: .zero)
+        .preferredColorScheme(.dark)
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
